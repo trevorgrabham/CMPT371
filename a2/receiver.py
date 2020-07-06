dc8479fbc3f35d5edc428250ebb5babe03a5133e
Trevor <tgrabham@sfu.ca>
2020-07-05 22:59:57 -0700

    a2

--- a2/receiver.py
+++ a2/receiver.py
@@ -2,9 +2,9 @@ from socket import *
 import random
 import sys
 
-seed = sys.argv[1]
-prob = sys.argv[2]
-randomGen = Random()
+seed = int(input('Enter value to seed the random number generator: '))
+prob = float(input('Enter the probability of a segment corruption: '))
+randomGen = random.Random()
 randomGen.seed(seed)
 serverHost = '127.0.0.1'
 serverPort = 50007
@@ -18,26 +18,26 @@ while True:
     if randomNum < prob:
         print('A Corrupted segment has been received')
         corrupt = 1
-        print('The receiver is moving back to state WAIT FOR' + expected + 'FROM BELOW')
+        print('The receiver is moving back to state WAIT FOR',expected,'FROM BELOW')
     else:
         corrupt = 0
-        data, seqSeg, seqAck, isack = segment.decode()
-        if expected == seqSeg:
-            print('A segment with sequence number' + seqSeg + 'has been received')
-            print('Segment received contains: data' + data + 'seqSeg=' + seqSeg + 'seqAck=' + seqAck + 'isack=' isack)
+        data, seqSeg, seqAck, isack = segment.decode().replace(' ','').split(',')
+        if expected == int(seqSeg):
+            print('A segment with sequence number',seqSeg,'has been received')
+            print('Segment received contains: data',data,'seqSeg=',seqSeg,'seqAck=',seqAck,'isack=',isack)
             if expected == 1:
                 print('An ACK1 is about to be sent')
-                ACK = (0,expected,1,1)
+                ACK = str((0,expected,1,1))[1:-1]
                 print('ACK to send contains: data = 0 seqSeg = 0 seqAck = 1 isack = 1')
                 expected = 0
             else:
                 print('An ACK0 is about to be sent')
-                ACK = (0,expected,0,1)
+                ACK = str((0,expected,0,1))[1:-1]
                 print('ACK to send contains: data = 0 seqSeg = 1 seqAck = 0 isack = 1')
                 expected = 1
-            print('The receiver is moving to state WAIT FOR' + expected + 'FROM BELOW')
+            print('The receiver is moving to state WAIT FOR',expected,'FROM BELOW')
             serverSocket.sendto(ACK.encode(),clientAddress)
         else:
-            print('A duplicate segment with sequence number' + seqSeg + 'has been received')
-            print('Segment received contains: data' + data + 'seqSeg=' + seqSeg + 'seqAck=' + seqAck + 'isack=' isack)
+            print('A duplicate segment with sequence number',seqSeg,'has been received')
+            print('Segment received contains: data',data,'seqSeg=',seqSeg,'seqAck=',seqAck,'isack=',isack)
     
--- a2/sender.py
+++ a2/sender.py
@@ -1,48 +1,63 @@
 from socket import *
 import random
 import sys
+import time
 
-timeSeed = sys.argv[1]
-numSegs = sys.argv[2]
-ackSeed = sys.argv[3]
-prob = sys.argv[4]
-dataSeed = sys.argv[5]
-rtt = sys.argv[6]
-dataGen = Random()
+timeSeed = int(input('Enter value to seed the time random number generator: '))
+numSegs = int(input('Enter the number of segments that are to be sent: '))
+ackSeed = int(input('Enter the value to seed the random number generator for a corrupted ACK: '))
+prob = float(input('Enter the proability that the ACK has been corrupted: '))
+dataSeed = int(input('Enter the value to seed the random number generator for the data: '))
+rtt = float(input('Enter the value of the round trip time: '))
+dataGen = random.Random()
 dataGen.seed(dataSeed)
-timeGen = Random()
+timeGen = random.Random()
 timeGen.seed(timeSeed)
-ackGen = Random()
+ackGen = random.Random()
 ackGen.seed(ackSeed)
 serverName = "127.0.0.1"
 serverPort = 50007
 clientSocket = socket(AF_INET,SOCK_DGRAM)
-
-for i in range(numSegs):
+startTime = time.time()
+sendTime = timeGen.random() * 5
+i = 0
+while i < numSegs:
+    if (time.time() - startTime) < sendTime:
+        continue
     seqSeg = i%2
-    print('A data segment with sequence number' + seqSeg + 'is about to be sent')
+    print('A data segment with sequence number',seqSeg,'is about to be sent')
     data = int(dataGen.random() * 1024)
     seqAck = 0
     isack = 0
-    print('Segment sent: data =' + data +  'seqSeg=' + seqSeg +  'seqAck = 0 isack = 0')
-    print('The sender is moving to state WAIT FOR ACK' + seqSeg)
-    segment = (data,seqSeg,seqAck,isack)
-    clientSocket.sendto(segment.encode(),(serverName,serverPort))
-    responseSeg, serverAddress = clientSocket.recvfrom(2048)
-    corrupt = ackSeed.random() < prob
+    timeoutTime = rtt
+    while (time.time() - timeoutTime) >= rtt:
+        try:
+            print('Segment sent: data =',data,'seqSeg=',seqSeg,'seqAck = 0 isack = 0')
+            print('The sender is moving to state WAIT FOR ACK',seqSeg)
+            segment = (data,seqSeg,seqAck,isack)
+            segment = str(segment)[1:-1]
+            clientSocket.sendto(segment.encode(),(serverName,serverPort))
+            timeoutTime = time.time()
+            clientSocket.settimeout(rtt)             
+            responseSeg, serverAddress = clientSocket.recvfrom(2048)
+            clientSocket.settimeout(None)  
+        except:
+            pass
+    corrupt = ackGen.random() < prob
     while corrupt:
         print('A Corrupted ACK segment has just been received')
-        print('The sender is moving back to state WAIT FOR ACK' + seqSeg)
-        print('A data segment with sequence number' + seqSeg + 'is about to be resent')
+        print('The sender is moving back to state WAIT FOR ACK',seqSeg)
+        print('A data segment with sequence number',seqSeg,'is about to be resent')
         corrupt = ackSeed.random() < prob
-    recvData, recvSeqSeg, recvSeqAck, recvIsack = responseSeg.decode()
+    recvData, recvSeqSeg, recvSeqAck, recvIsack = responseSeg.decode().replace(' ','').split(',')
     if recvSeqAck == 1:
         print('An ACK1 segment has just been received')
     else:
         print('An ACK0 segment has just been received')
-    print('ACK received: data =' + recvData + 'seqSeg =' + recvSeqSeg + 'seqAck =' + recvSeqAck + 'isAck = 1')
-    print('The sender is moving to state WAIT FOR CALL' + (seqSeg+1)%2 + 'FROM ABOVE') 
+    print('ACK received: data =',recvData,'seqSeg =',recvSeqSeg,'seqAck =',recvSeqAck,'isAck = 1')
+    print('The sender is moving to state WAIT FOR CALL',(seqSeg+1)%2,'FROM ABOVE') 
+    i += 1
+    sendTime += (timeGen.random()*5)
     
 clientSocket.close()
 
-# no idea what to do about timing...
